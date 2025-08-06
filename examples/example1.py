efafe4f79703726f1a7997c5bb65410b801685fd
Jernej Kolar <jernej.kolar.jk@gmail.com>
2025-08-06 21:38:38 +0200

    Update example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -1,23 +1,35 @@
-import sdypy as sd
+import sdypy_dev as sd
 import meshio
 import numpy as np
 
-mesh = meshio.read("L_bracket.msh")
+mesh = meshio.read("./examples/L_bracket.msh")
 
-# extract nodes and elements from mesh
 nodes = mesh.points
 elements = []
 for cells in mesh.cells:
     if cells.type == "quad":
         elements.append(cells.data)
+
 elements = np.vstack(elements)
 
-
 mode_shape = np.zeros((nodes.shape[0], 3))
-mode_shape[:, 0] = 10.0
+mode_shape[:, 0] = 10
 
 plotter = sd.view.Plotter3D()
-plotter.add_fem_mesh(nodes, elements, animate=mode_shape, field="norm")
-plotter.show(show_grid=True, show_axes=True)
+plotter.animate = mode_shape
+plotter.n_frames = 400
+plotter.default_accelerometer_size = np.array([10, 10, 5])
+plotter.default_excitation_size = 50
+plotter.snap_to_closest_node = True
+
+plotter.add_fem_mesh(nodes, elements, animate=mode_shape, field=None)
+plotter.add_axes()
+
+import sys
+from PyQt5.QtWidgets import QApplication
+app = QApplication.instance()
+if app is None:
+    app = QApplication(sys.argv)
+app.exec_()
 
 
